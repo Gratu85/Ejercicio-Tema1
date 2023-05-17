701fe878d8e8f1699d33971919435153b9b5da6a
Jordi Gratovil <firesgratovil@hotmail.com>
2023-05-17 21:47:50 +0200

    hola

---
+++ EjercicioBucle/src/BucleWhile.java
@@ -0,0 +1,21 @@
+public class BucleWhile {
+    public static void main(String[] args) {
+
+        String[] Nombres = {"Oriol", "Francesc", "Antonio", "Raquel", "Joana"};
+
+        int contador=0;
+
+        while (contador<Nombres.length) {
+            for (String nombre : Nombres) {
+                String cadena = "";
+                System.out.printf(cadena);
+                System.out.printf(nombre);
+                contador++;
+
+            }
+
+        }
+    }
+}
+
+
